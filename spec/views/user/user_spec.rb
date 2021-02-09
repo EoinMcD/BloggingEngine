7aabb777b135d22cacf2c9397164dad7ec05baaf
EoinMcD <eoin.mcdonough@wpengine.com>
2021-02-09 14:17:16 +0000

    test: :white_check_mark: Added test to check if page is shown

--- spec/views/user/user_spec.rb
+++ spec/views/user/user_spec.rb
@@ -4,6 +4,17 @@ require 'rails_helper'
 
 
 
+RSpec.describe UserController, type: :controller do
+  describe 'GET #User' do
+    context 'when viewing homepage in the browser' do
+      it 'returns a 200 OK response' do
+        get :User
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+end
+
 RSpec.describe 'User', type: :feature do
   it 'Shows text' do
     visit '/user'
