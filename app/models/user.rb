0680536eb3370f3518df5d263e427638e92b3d10
EoinMcD <eoin.mcdonough@wpengine.com>
2021-02-18 13:38:28 +0000

    feat: Added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
-    validates :first_name, presence: true, length:{maximum: 50}
+    validates :first_name, presence: true, length:{maximum: 25}
+    validates :second_name, presence: true, length:{maximum: 25}
 
     before_save {self.email = email.downcase}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,5 +9,10 @@ class User < ApplicationRecord
     validates :email, presence: true, length: {maximum:255},
                             format: {with:VALID_EMAIL_REGEX},
                             uniqueness: {case_sensitive:false}
+
+
+    has_secure_password
+
+    validates :password, length: { minimum: 6 } ,presence: true
 
 end
