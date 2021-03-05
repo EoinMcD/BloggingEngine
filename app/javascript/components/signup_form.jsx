3883cea3ed897deed82d62770c927768a8b3d42e
EoinMcD <eoin.mcdonough@wpengine.com>
2021-03-05 15:10:27 +0000

    refactor: Converted class component to functional component

--- app/javascript/components/signup_form.jsx
+++ app/javascript/components/signup_form.jsx
@@ -3,116 +3,71 @@ import ReactDOM, { render } from 'react-dom'
 import $ from 'jquery' 
 
 
-class SignupForm extends React.Component {
-    
-    static errorsList;
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            sname: '',
-            email: '',
-            password: '',
-            confirmpassword: '',
-            errors: '',
-        };
+function SignupForm(){
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.SendForm = this.SendForm.bind(this);
-        
-    }
-    
-   
-    
-    handleChange(event) {
-        console.log(event)
-        this.setState({ [event.target.name] : event.target.value });
-        
-    }
-    
-    handleSubmit(event) {
-        
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-    }
-    
-    SendForm= function (e) {
+    const [name, setName] = React.useState("");
+    const [sname, setSname] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [confirmpassword, setConfirm] = React.useState("");
+    const [errorsList, setErrors] = React.useState("");
+
+    const SendForm = (e) => {
         e.preventDefault();
-        
-        var list = []
           $.ajax({
             async: false,
             type: 'POST',
             url: '/create',
-            data: {first_name: this.state.name,second_name: this.state.sname, email: this.state.email, password: this.state.password, password_confirmation: this.state.confirmpassword},
+            data: { first_name: name,
+                second_name: sname, 
+                email: email,
+                password: password, 
+                password_confirmation: confirmpassword },
+
             success: function (result){
-                console.log("Success")
-                list = result.errors
-                console.log("LIST   " +list.length)
-                this.errorsList =  new Array();
-                for(var i=0;i<list.length;i++) {
-                    console.log("PASSED   "+list[i])
-                    this.errorsList.push(list[i]);
-                }
-                
-                console.log("ERRORS:  " + this.errorsList)   
+                setErrors(result.errors)
             },
-            error: function (result) { 
-                console.log("Fail")                       
-                console.log(result)
-                console.log(result.error)
+            error: function (result) {                        
             }
         })
-        console.log("RE RENDERING")
-        this.setState({ mssg: "Hi there!" });
-        
-        
     }
-
-    render () {
-        console.log(this.state)
-        console.log("TESTING" + this.errorsList)
-        
         return(
-            <form ref="form" onSubmit={this.SendForm}>
-                <div style ={{width:50,height:50}}>
-                    {this.errorsList}
-                </div>
-                
-                <div style ={{width:50,height:50}}>
-                <fieldset>
-                    <label>
-                       First Name:
-                       <input className="form-control" name="name" value={this.state.name} onChange={this.handleChange} />
-                       
+            <div>
+            <form onSubmit={SendForm}>
+                {errorsList ? <div>{errorsList}</div> : null}
+                    <div style ={{width:50,height:50}}>
+                        <fieldset>
+                            <label>
+                            First Name:
+                        <input className="form-control" name="name" onChange={(e) => setName(e.target.value)} />
                     </label>
                     <label>
                         Second Name:
-                        <input className="form-control" name="sname" value={this.state.sname} onChange={this.handleChange} />
+                        <input className="form-control" name="sname" onChange={(e) => setSname(e.target.value)}/>
                     </label>
                     <label>
                         Email:
-                        <input className="form-control" name="email" value={this.state.email} onChange={this.handleChange} />
+                        <input className="form-control" name="email" onChange={(e) => setEmail(e.target.value)} />
                     </label>
                     <label>
                         Password:
-                        <input className="form-control" type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                        <input className="form-control" type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
                     </label>
                     <label>
-                        Confirm Password:
-                        <input className="form-control" type="password" name="confirmpassword" value={this.state.confirmpassword} onChange={this.handleChange} />
+                        Confirm Password: 
+                        <input className="form-control" type="password" name="confirmpassword" onChange={(e) => setConfirm(e.target.value)} />
                     </label>
                     <button type='submit'>Submit</button>
                 </fieldset>
                 </div>
                 
             </form>
-            
+            </div>
+             
             
         )
     }
-}
+
 
 function RenderErrors(props) {
     return <h1>{props.error}</h1>
